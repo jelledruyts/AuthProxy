08b1459e88639bf46f70ba7d85b08dbcad3ff76b
Jelle Druyts <jelle_druyts@yahoo.com>
2022-03-25 09:21:50 +0100

    Add ideas on claim transformation expressions

--- src/AuthProxy/Infrastructure/ClaimsTransformer.cs
+++ src/AuthProxy/Infrastructure/ClaimsTransformer.cs
@@ -3,7 +3,26 @@ using AuthProxy.Configuration;
 
 namespace AuthProxy.Infrastructure;
 
-// TODO: Add a claims transformation which can split a claim value into multiple claims, for example space-separated "scope" values.
+// TODO: Change claims transformations with expression syntax
+//   Inputs: claims (claim[roles]), configuration/metadata (config[idp.name]), constant strings (string['foo'])
+//   Functions: +, split, join
+// Examples:
+// - "roles=claim[http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname]" => returns all "surname" claim values with their original claim type
+// - "roles=claim[roles]" => returns all "roles" claim values with their original claim type
+//     Example: ( "roles": "read", "roles": "write" ) => ( "roles": "read", "roles": "write" )
+// - "roles=roles" => returns all "roles" claim values with their original claim type (shorter syntax to simplify "claim[<name>]")
+// - "roles" => returns all "roles" claim values with their original claim type (short hand syntax)
+// - "ver=string['1.0']" => returns a "ver" claim value with a constant string
+// - "ver='1.0'" => returns a "ver" claim value with a constant string (shorter syntax where quotes imply string[])
+// - "scp=split(scp, ' ')" => returns all "scp" claim values and for each value splits the string into multiple values
+//     Example: ( "scp": "a b c", "scp": "a d" ) => ( "scp: "a", "scp: "b", "scp: "c", "scp: "a", "scp: "d" )
+// - "roles=join(roles, ' ')" => returns one "roles" claim value with the concatenated values of all original "roles" claim values
+//     Example: ( "roles": "read", "roles": "write" ) => ( "roles": "read write" )
+// - "idp-name=idp[name]" => returns a claim with a configuration value of the identity provider which authenticated the user
+// - "sub=sub + '@' + iss" => returns claims with a concatenation of the "sub" and "iss" claim values (cartesian product of all input claim values)
+//     Example: ( "sub": "a", "sub": "b", "iss": "x", "iss": "y" ) => ( "sub": "a@x", "sub": "a@y", "sub": "b@x", "sub": "b@y" )
+// - "sub=join(sub, ' ') + '@' + join(iss, ' ')" => returns a single claims with a concatenation of the joined "sub" and "iss" claim values
+//     Example: ( "sub": "a", "sub": "b", "iss": "x", "iss": "y" ) => ( "sub": "a b@x y" )
 public class ClaimsTransformer
 {
     private readonly IdentityProviderConfig identityProvider;
@@ -70,12 +89,6 @@ public class ClaimsTransformer
         // Add a specific claim for the "subject".
         var inputSubjectClaims = this.inputSubjectClaimTypes.SelectMany(inputSubjectClaimType => claims.Where(c => c.Type == inputSubjectClaimType).Select(c => c.Value));
         output.Add(new Claim(this.outputSubjectClaimType, string.Join(this.SubjectClaimValueSeparator, inputSubjectClaims)));
-
-        // TODO: Add information about the IdP that authenticated the user.
-        // This and the "subject" transformation could be done with configurable expressions such as:
-        //   "sub=claim:sub + '@' + claim:iss)"
-        //   "auth-idp-name=idp:name"
-        //   "auth-idp-type=idp:type"
 
         return Task.FromResult<IEnumerable<Claim>>(output);
     }
